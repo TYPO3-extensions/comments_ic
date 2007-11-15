034f06592dfc48e60ebd11fd3de0a777f564d41c
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-11-15 10:56:58 +0000

    "EM update"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments_ic/trunk@7239 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "comments_ic"
 #
-# Auto generated 15-11-2007 11:34
+# Auto generated 15-11-2007 12:56
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-',
+			'php' => '5.0.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
@@ -41,7 +41,9 @@ $EM_CONF[$_EXTKEY] = array(
 			'comments' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"0a42";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"25aa";s:14:"ext_tables.sql";s:4:"da71";s:16:"locallang_db.xml";s:4:"68e3";s:19:"doc/wizard_form.dat";s:4:"2e0d";s:20:"doc/wizard_form.html";s:4:"fba8";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"c3c9";s:29:"class.tx_commentsic_hooks.php";s:4:"acc0";s:12:"ext_icon.gif";s:4:"ee94";s:17:"ext_localconf.php";s:4:"0c27";s:14:"ext_tables.php";s:4:"ac74";s:14:"ext_tables.sql";s:4:"9a75";s:16:"locallang_db.xml";s:4:"705d";}',
+	'suggests' => array(
+	),
 );
 
 ?>

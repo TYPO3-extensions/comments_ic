3622f02f53d2a63d10f7dcd1dc30b6bcd7072dc9
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-01-05 15:52:10 +0000

    Changed name and company
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments_ic/trunk@7828 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "comments_ic"
 #
-# Auto generated 19-11-2007 12:20
+# Auto generated 10-12-2007 17:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Comments: individual closing',
 	'description' => 'Adds individual closing of comments for tt_news and commerce products',
 	'category' => 'fe',
-	'author' => 'Dmitry Dulepov',
+	'author' => 'Dmitry Dulepov [netcreators]',
 	'author_email' => 'dmitry@typo3.org',
 	'shy' => '',
 	'dependencies' => 'comments',
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => 'tt_news,tx_commerce_products',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author_company' => '',
-	'version' => '1.0.1',
+	'author_company' => 'Netcreators BV',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'commerce' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"c3c9";s:29:"class.tx_commentsic_hooks.php";s:4:"acc0";s:12:"ext_icon.gif";s:4:"ee94";s:17:"ext_localconf.php";s:4:"0c27";s:14:"ext_tables.php";s:4:"ac74";s:14:"ext_tables.sql";s:4:"9a75";s:16:"locallang_db.xml";s:4:"705d";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"fbf2";s:29:"class.tx_commentsic_hooks.php";s:4:"acc0";s:12:"ext_icon.gif";s:4:"ee94";s:17:"ext_localconf.php";s:4:"1470";s:14:"ext_tables.php";s:4:"ac74";s:14:"ext_tables.sql";s:4:"9a75";s:16:"locallang_db.xml";s:4:"705d";}',
 	'suggests' => array(
 	),
 );
